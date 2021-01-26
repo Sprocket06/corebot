08ddc800a290684ef36f09a4d8691d57dad58213
Sprocket06 <24927632+Sprocket06@users.noreply.github.com>
2021-01-26 16:17:53 +0000

    I am dumb

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const client = new Discord.Client();
 const config = require('./config.json')
 //load command handlers
 var Handlers = requireDir('./handlers', { noCache:true })
+var LogChannel
 
 client.login(config.token)
 
 client.on('ready', _=>{
 	console.log('discord link online')
+	LogChannel = client.channels.fetch(config.LogChannel)
 });
 
 client.on('message', msg => {
@@ -16,11 +18,12 @@ client.on('message', msg => {
 		let args = msg.content.split(' ');
 		if(Handlers[args[0].slice(1)]){
 			console.log(`Handling ${args.join(' ')}`)
+			LogChannel.send(`Handling ${args.join(' ')}`)
 			try{
 				Handlers[args[0].slice(1)](args, msg);
 			}catch(e){
 				console.log(e)
-				client.users.fetch(config.admin).send(e)
+				LogChannel.send(e.stack);
 				msg.channel.send('There was an error in processing your command.')
 			}
 		}else if(args[0] == '!reloadshit'){
