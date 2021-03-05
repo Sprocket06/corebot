c388f87a5ef8fb395e0fb39a7c61388f8bdd20e8
Sprocket06 <24927632+Sprocket06@users.noreply.github.com>
2021-03-05 15:56:47 -0500

    support for live testing in-development commands

--- commandManager.js
+++ commandManager.js
@@ -1,19 +1,31 @@
+const config = require('./config.json')
+
 class CommandManager {
   constructor(){
     this.handlers = {}
     this.logChannel
+    this.experimentalCommands = []
   }
   handleMessage(msg){
     let args = msg.content.replace(/\s{2,}/g," ").split(' ')
       , cmd = args[0];
     if(this.handlers[cmd]){
-      this.handlers[cmd](args,msg);
       let l = `user: ${msg.author.username} (id: ${msg.author.id}) cmd: ${msg.content}`
       global.log(l)
+      if(this.experimentalCommands.includes(cmd)){
+        if(msg.guild && config.test_servers.includes(msg.guild.id)){
+          this.handlers[cmd](args,msg);
+        }
+      }else{
+        this.handlers[cmd](args,msg);
+      }
     }
   }
-  addHandler(name,fn){
+  addHandler(name,fn,isTestFeature){
     this.handlers[name] = fn
+    if(isTestFeature){
+      this.experimentalCommands.push(name)
+    }
   }
   removeHandler(name){
     delete this.handlers[name];
