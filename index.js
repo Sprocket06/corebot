bc65b06ebbbf144536b01ca81359e6c3feaac7f0
Sprocket06 <24927632+Sprocket06@users.noreply.github.com>
2021-05-30 22:09:55 -0400

    card mentions!

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const config = require('./config.json')
 //load command handlers
 var Handlers = requireDir('./handlers', { noCache:true })
 var CommandManager = require('./commandManager.js');
+var {tokens, cards} = require('./cardSearch.js');
 var LogChannel
 var RedemptionChannel
+
+function formatLeadText(leader){
+	let leadtext = leader
+	let leads = leader.match(/[A-Z]/g)
+	if(leads.length > 1){
+		let i = leader.indexOf(leads[1])
+		leadtext = `${leadtext.substring(0,i)} / ${leadtext.substring(i)}`
+	}
+	return leadtext
+}
 
 global.self = client
 
@@ -24,6 +35,19 @@ global.sendRedemptionMsg = function(text){
 	}
 }
 
+//this is a bit scuffed but the current framework doesn't allow for this type of feature
+//thus, we implement it at a slightly loewr level than normal
+function parseCardMentions(input){
+	var r = /\[\[([ \w]+)\]\]/g
+	  , cName = r.exec(input)
+		, cNames = []
+	while(cName != null){
+		cNames.push(cName[1].trim())
+		cName = r.exec(input)
+	}
+	return cNames
+}
+
 client.login(config.token)
 
 client.on('ready', _=>{
@@ -38,6 +62,28 @@ client.on('message', msg => {
 		if(msg.author.id == config.admin){
 			Handlers = requireDir('./handlers', { noCache:true });
 			msg.reply('done.')
+		}
+	}else if(!msg.content.startsWith('!')){
+		var cMentions = parseCardMentions(msg.content)
+		if(cMentions.length){
+			cMentions.forEach(cName => {
+				var card = cards.search(cName)[0]
+				var token = tokens.search(cName)[0]
+				if(card && token){
+					if(token.score > card.score){
+						card = token
+					}
+				}
+				if(!card){
+					msg.channel.send(`You mentioned the card ${cName}, but I could not find a matching database entry.`)
+				}else{
+					var data = card.item;
+					let embed = new Discord.MessageEmbed()
+			      .setTitle(`${data.name} (${data.rating})`)
+			      .setDescription(`${formatLeadText(data.leader)} ${data.creature?`**${data.power}/${data.hp}**`:`**Spell**`}\n${data.cardtext?`*${data.cardtext}*`:''}${data.tokens?`${data.tokens.map(_=>`\n${_.name}: ${_.power}/${_.hp} ${_.cardtext?`*${_.cardtext}*`:''}`)}`:''}`)
+					msg.channel.send(embed)
+				}
+			})
 		}
 	}else{
 		try {
